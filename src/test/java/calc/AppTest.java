62f5a4510af10838f2ce42dc7fa5f7438d06474b
Majaalex <alex-maja@hotmail.com>
2019-02-21 16:57:35 +0200

    Adding testing

--- src/test/java/calc/AppTest.java
+++ src/test/java/calc/AppTest.java
@@ -4,6 +4,11 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 /**
  * Unit test for simple App.
  */
@@ -32,29 +37,50 @@ public class AppTest
      * Rigourous Test :-)
      */
     public void testApp() {
+
         System.out.println("Testing power");
         //testPower(10,2);
         //testPower(-5,5);
-        testPower(3.0,-5.0);
+        try {
+            File input = new File("src/test/resources/numbers.txt");
+            Scanner in = new Scanner(input);
+            while (in.hasNextDouble()){
+
+                double a = in.nextDouble();
+                System.out.println("a is " + a);
+                double b = in.nextDouble();
+                System.out.println("b is " + b);
+                double result = Math.pow(a,b);
+                testPower(a,b,result);
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
-    private void testPower(double base, double power) {
+    private void testPower(double base, double power, double expected) {
+        DecimalFormat df = new DecimalFormat("#.##########");
         // if power == 0 result will be 1, startValue is 1 since 0*X = 0
         double output = Math.pow(base,power);
         System.out.println(output);
         double result = 1.0;
 
         // a^-1 == (1/a)^1
-        /*if (power < 0){
+        if (power < 0){
             base = 1 / base;
             power = -power;
-        }*/
+        }
 
         // multiplies the value by itself power times
         for (int i = 0; i < power; i++){
             result *= base;
         }
-        assertEquals(Math.pow(base,power),result);
+        expected = Double.parseDouble(df.format(expected));
+        result = Double.parseDouble(df.format(result));
+        assertEquals(expected,result);
+
+
     }
 
 
